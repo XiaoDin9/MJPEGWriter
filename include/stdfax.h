4f885bdcaeea88d268a57a7b75c4f22e607a49b9
ding-yong <yding1025@163.com>
2022-01-13 11:01:53 +0800

    Create stdfax.h

---
+++ include/stdfax.h
@@ -0,0 +1,19 @@
+//
+// Created by 6000022894 on 2021/11/29.
+//
+
+#ifndef DEMO_MAIN_STDFAX_H
+#define DEMO_MAIN_STDFAX_H
+
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+#include <iostream>
+#include <ctime>
+#include <boost/filesystem.hpp>
+#include <vector>
+
+using namespace cv;
+using namespace std;
+using namespace boost::filesystem;
+
+#endif //DEMO_MAIN_STDFAX_H
